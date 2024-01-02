85530913db8ad7841cd9a9058a26597be008493c
Stijn Rogiest <reddusted@gmail.com>
2024-01-02 13:03:13 +0100

    Install prisma & FontAwesome

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import { Button, Text, Center, Heading } from "@chakra-ui/react";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function Home() {
-    return <main>this is the home page</main>;
+    return (
+        <Center style={{ height: "100%", flexDirection: "column", gap: "1em" }}>
+            <Heading as="h1">Pay Up!</Heading>
+            <Button size="lg" colorScheme="orange" rightIcon={<FontAwesomeIcon icon={faArrowRight} />}>
+                Create Payment Request
+            </Button>
+            <Text style={{ opacity: "0.5" }}>You'll need to log in using Google to create a payment request.</Text>
+        </Center>
+    );
 }
