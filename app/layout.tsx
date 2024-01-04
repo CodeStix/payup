735e92424507429c2dc031fc80cc7807fbba600d
Stijn Rogiest <reddusted@gmail.com>
2024-01-04 20:47:28 +0100

    Add opengraph meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Providers } from "./providers";
 
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import Head from "next/head";
 config.autoAddCss = false;
 
 const rubik = Rubik({ subsets: ["latin"] });
@@ -17,6 +18,17 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
+            <head>
+                <meta name="theme-color" content="#dd6b20" />
+                <meta name="description" content="Automatically get paid and reminded about payments." />
+                <link rel="apple-touch-icon" href="/icon.png" />
+                <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
+                <meta property="og:title" content="ConsumerHouse Onderzoeken" />
+                <meta property="og:description" content="Automatically get paid and reminded about payments." />
+                <meta property="og:url" content="https://payup.weboot.nl/" />
+                {/* <meta property="og:image" content="https://payup.weboot.nl/banner.jpg" /> */}
+                <title>Pay Up!</title>
+            </head>
             <body className={rubik.className}>
                 <Providers>{children}</Providers>
             </body>
